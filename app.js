27f5b5d18effdba725fa7cf7f4a4cc62d0268b44
huangjiankang <huangjiankang@yunlizhihui.com>
2019-06-17 19:41:07 +0800

    添加跨域实例

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('koa-bodyparser');
 const session = require('koa-session');
 const koaStatic = require('koa-static');
+const cors = require('koa2-cors');
 const app = new koa();
 const user = require('./router/user')
 //session cookie 加密信息
@@ -16,7 +17,17 @@ const sessionConfig = {
     rolling: true,//设置为 true 刷新页面重新计时
     signed: true
 }
-
+const corsConfig = {
+    origin: function(ctx) {//根据请求 url 限制是否允许跨域,也可直接设置为‘*’允许所有跨域
+        if (ctx.url === '/postTest') {
+            return '*'
+        }
+        return false;
+    },
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE','PUT','OPTIONS']//允许的请求方法
+}
 
 //处理错误信息,发送错误码
 const err = async(ctx) => {
@@ -27,6 +38,8 @@ const err = async(ctx) => {
     } 
 }
 app
+    //跨域配置
+    .use(cors(corsConfig))
     //session 中间件
     .use(session(sessionConfig, app))
     //渲染前端页面 模板引擎为 ejs | html
--- router/user.js
+++ router/user.js
@@ -6,10 +6,25 @@ router.get('/', async ctx => {
 
 router.get('/post',ctx => {
     //设置 session
+    let { a,b } = ctx.request.query;
     ctx.session.test = Date.now();
     ctx.body = {
         code:200,
         msg:'post success'
+    }
+})
+
+router.post('/postTest',async ctx => {
+    let { a , b } = ctx.request.body;
+    ctx.response.body = {
+        a,b
+    }
+})
+
+router.post('/post',async ctx => {
+    let { a , b } = ctx.request.body;
+    ctx.response.body = {
+        a,b
     }
 })
 
